2dfa509c5c23cb75964e3bb76f5dd2a9efe0465d
Jeff Smith <jffreyjs@gmail.com>
2015-03-23 21:50:47 -0400

    scaffolding

--- src/index.js
+++ src/index.js
@@ -1,13 +1,47 @@
 (function() {
-  var opcode, // 2 bytes
+  var opcode,                   // 2 bytes
+      I,                        // Index register 
+      pc,                       // program counter
+      delayTimer,
+      soundTimer,
+      sp,                       // stack pointer
       memory = new Array(4096), // 4K memory
-      V = new Array(16), // general purpose registers
-      I,  // Index register 
-      pc, // program counter
-      gfx = new Array(64) // 64 x 32
-  ;
+      stack  = new Array(16),
+      V      = new Array(16),   // general purpose registers
+      key    = new Array(16),   // keypad
+      gfx    = new Array(64);   // 64 x 32
 
   for(var i = 0; i < gfx.length; i++) {
     gfx[i] = new Array(32);
   }
+
+  function initialize() {
+    
+  }
+
+  function load() {
+    
+  }
+
+  function emulateCycle() {
+    
+  }
+
+  function draw() {
+    
+  }
+
+  function setKeys() {
+    
+  }
+
+  function run() {
+    
+  }
+
+  return {
+    initialize: initialize,
+    load: load,
+    run: run
+  };
 })();
