f5fcd4b5aab86eeb9ca49f238a8940fef235d328
jffjs <jffreyjs@gmail.com>
2015-03-24 17:00:53 -0400

    initialize and load, specs

--- src/index.js
+++ src/index.js
@@ -1,85 +1,100 @@
-(function() {
-  function Chip8() {
-    this.opcode     = 0;               // 2 bytes
-    this.I          = 0;               // Index register 
-    this.pc         = 0;               // program counter
-    this.delayTimer = 0;
-    this.soundTimer = 0;
-    this.sp         = 0;               // stack pointer
-    this.memory     = new Array(4096); // 4K memory
-    this.stack      = new Array(16);
-    this.V          = new Array(16);   // general purpose registers
-    this.key        = new Array(16);   // keypad
-    this.gfx        = new Array(64);   // 64 x 32
+var fontSet = new Uint8Array([
+  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+  0x20, 0x60, 0x20, 0x20, 0x70, // 1
+  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+  0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+  0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+  0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+  0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+  0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+  0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+]);
 
-    function clearRegisters() {
-      var i;
-      for(i = 0; i < this.gfx.length; i++) {
-        this.gfx[i] = new Array(32);
-        for(var j = 0; j < this.gfx[i].length; j++) {
-          this.gfx[i][j] = 0;
-        }
-      }
+/**
+ * CHIP-8 Emulator
+ * @constructor
+ */
+function Chip8() {
+  this.opcode     = 0;                    // 2 bytes
+  this.I          = 0;                    // Index register 
+  this.pc         = 0;                    // program counter
+  this.delayTimer = 0;
+  this.soundTimer = 0;
+  this.sp         = 0;                    // stack pointer
+  this.memory     = new Uint8Array(4096); // 4K memory
+  this.stack      = new Uint16Array(16);
+  this.V          = new Uint8Array(16);   // general purpose registers
+  this.key        = new Uint8Array(16);   // keypad
+  this.gfx        = new Array(64);        // 64 x 32
 
-      for(i = 0; i < this.memory.length; i++) {
-        this.memory[i] = 0;
-      }
-
-      for(i = 0; i < 16; i++) {
-        this.stack[i] = 0;
-        this.V[i] = 0;
-        this.key[i] = 0;
-      }
-    }
-
-    this.initialize = function() {
-      this.pc     = 0x200;  // Program counter starts at 0x200
-      this.opcode = 0;      // Reset current opcode	
-      this.I      = 0;      // Reset index register
-      this.sp     = 0;      // Reset stack pointer
-      clearRegisters();
-
-      // Load font set
-    };
-
-    this.load = function() {
-      
-    };
-
-    this.draw = function() {
-      
-    };
-
-    this.setKeys = function() {
-      
-    };
-
-    this.emulateCycle = function() {
-      // Fetch opcode
-      // Decode opcode
-      // Execute opcode
-      
-      // Update timers
-    };
-
-    this.run = function() {
-      var drawFlag = false;
-      var loop = function() {
-        for(var i = 0; i < 10; i++) {
-          this.emulateCycle();
-
-          if (drawFlag) {
-            this.draw();
-          }
-
-          this.setKeys();
-        }
-        window.requestAnimationFrame(loop);
-      }.bind(this);
-
-      window.requestAnimationFrame(loop);
-    };
+  for(i = 0; i < this.gfx.length; i++) {
+    this.gfx[i] = new Uint8Array(32);
   }
 
-  return new Chip8();
-})();
+  this.initialize = function() {
+    this.pc     = 0x200;  // Program counter starts at 0x200
+    this.opcode = 0;      // Reset current opcode	
+    this.I      = 0;      // Reset index register
+    this.sp     = 0;      // Reset stack pointer
+
+    // Load font set
+    for (var i = 0; i < 80; i++) {
+      this.memory[i] = fontSet[i];
+    }
+  };
+
+  /**
+   * Loads buffer into memory
+   * @param {Uint8Array} buffer - binary data to be loaded into memory
+   */
+  this.load = function(buffer) {
+    this.initialize();
+
+    for(var i = 0; i < buffer.byteLength; i++) {
+      this.memory[i + 512] = buffer[i];
+    }
+    
+  };
+
+  this.draw = function() {
+    
+  };
+
+  this.setKeys = function() {
+    
+  };
+
+  this.emulateCycle = function() {
+    // Fetch opcode
+    // Decode opcode
+    // Execute opcode
+    
+    // Update timers
+  };
+
+  this.run = function() {
+    var drawFlag = false;
+    var loop = function() {
+      for(var i = 0; i < 10; i++) {
+        this.emulateCycle();
+
+        if (drawFlag) {
+          this.draw();
+        }
+
+        this.setKeys();
+      }
+      window.requestAnimationFrame(loop);
+    }.bind(this);
+
+    window.requestAnimationFrame(loop);
+  };
+}
+
--- test/chip8Spec.js
+++ test/chip8Spec.js
@@ -1,6 +1,50 @@
 describe("CHIP-8", function() {
+  var chip8;
   
-  it("does something", function() {
-    console.log("hello world");
+  beforeEach(function() {
+    chip8 = new Chip8();
+    chip8.initialize();
+  });
+
+  describe("initialize", function() {
+    it("sets program counter to 0x200", function() {
+      expect(chip8.pc).toEqual(0x200);
+    });
+
+    it("resets current opcode", function() {
+      expect(chip8.opcode).toEqual(0);
+    });
+
+    it("resets index register", function() {
+      expect(chip8.I).toEqual(0);
+    });
+
+    it("resets stack pointer", function() {
+      expect(chip8.sp).toEqual(0);
+    });
+
+    it("loads font set into memory", function() {
+      for(var i = 0; i < fontSet.length; i++) {
+        expect(chip8.memory[i]).toEqual(fontSet[i]);
+      }
+    });
+  });
+
+  describe("load", function() {
+    var buffer = new Uint8Array([0xF8, 0x90, 0xAB, 0x78, 0x9C, 0xF8, 0x90, 0xAB, 0x78, 0x9C]);
+
+    it("initializes emulator", function() {
+      spyOn(chip8, 'initialize').and.callThrough();
+      chip8.load(buffer);
+      expect(chip8.initialize).toHaveBeenCalled();
+    });
+
+    it("loads the buffer into memory starting at address 0x200", function() {
+      chip8.load(buffer);
+
+      for(var i = 0; i < buffer.byteLength; i++) {
+        expect(chip8.memory[i + 512]).toEqual(buffer[i]);
+      }
+    });
   });
 });
